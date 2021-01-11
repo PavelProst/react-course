e8718d12ed4a4bedf2bc9639b31062ad70ead3e9
PavelProst <pavelprost@yahoo.com>
2021-01-11 19:34:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                   <div className="col col-lg-12">
                       <h1>First React APP</h1>
                       <img src="https://cdn0.iconfinder.com/data/icons/flat-round-system/512/reactos-256.png" alt=""/>
-                          <p>my first react app</p>
+                          <p>my first 1 react app</p>
                           <button className="button-primary">Default</button>
                   </div>
               </div>
